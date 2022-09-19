1663ff8b24d16038afc8b1431a2a82dd6270e14d
Matthew Moukaddem <matt.moukaddem@gmail.com>
2022-09-19 21:04:31 +1000

    commit

--- Starlight/API/migrations.sh
+++ Starlight/API/migrations.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
 
 dotnet ef migrations bundle --self-contained -r linux-x64 --verbose --configuration Bundle
-
-docker-compose -f docker-compose-migrations.yml build
-docker-compose -f docker-compose-migrations.yml push 
--- Starlight/API/publish.sh
+++ Starlight/API/publish.sh
@@ -1,2 +1,5 @@
 docker-compose build
 docker-compose push 
+
+docker-compose -f docker-compose-migrations.yml build
+docker-compose -f docker-compose-migrations.yml push 
